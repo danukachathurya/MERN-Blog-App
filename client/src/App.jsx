51dc891dfccc7933180b020ce913ea8a38a82a4f
Danuka Chathurya <danukachathurya8@gmail.com>
2024-06-14 20:05:09 +0530

    create pages and routes

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Signin from './pages/Signin';
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
