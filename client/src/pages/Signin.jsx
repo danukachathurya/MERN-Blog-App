a7200f946aa7f15774c170ace568fe261ba786ca
Danuka Chathurya <danukachathurya8@gmail.com>
2024-06-17 18:36:41 +0530

    Add redux persist

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -71,6 +71,7 @@ export default function SignIn() {
               Sign In
             </Button>
           </form>
+          {errorMessage && <p className="text-red-500 mt-2">{errorMessage}</p>}
           <div className="flex gap-2 text-sm mt-5">
             <span>Don't have an account?</span>
             <Link to="/sign-up" className="text-blue-500">
